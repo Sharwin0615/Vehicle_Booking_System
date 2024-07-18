bf4f0b85aac81d93ca739b0aacbef923b7fbbc2a
Sharwin0615 <sharwinmendonca8@gmail.com>
2024-07-18 19:56:44 +0530

    project

---
+++ vendor/inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Vehicle Booking System | By Sharwin And Shubhang   
+       </p>
+    </div>
+    <!-- /.container -->
+  </footer>
